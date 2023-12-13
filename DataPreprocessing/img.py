5757eded4a31ae6878da235e0775247de5d72d93
Hayeon88 <hayeon0808@swu.ac.kr>
2023-12-13 19:05:35 +0900

    ✨ feat : 이미지 경로 수정 및 이름 변경 #16 - dest_dir 경로 유무에 따라 디렉토리 생성

--- DataPreprocessing/img.py
+++ DataPreprocessing/img.py
@@ -7,7 +7,7 @@ img path 변경 & 이름 수정
 img_path, dest_dir 개인에 맞게 수정 필요
 """
 
-img_path = "../../train/images"
+img_path = "../../project1/data/train/images/"
 
 img_files = glob.glob(img_path + "/**/*.jpeg", recursive=True) + glob.glob(
     img_path + "/**/*.jpg", recursive=True
@@ -15,6 +15,9 @@ img_files = glob.glob(img_path + "/**/*.jpeg", recursive=True) + glob.glob(
 
 dest_dir = "../../images"
 
+if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+
 for img_file in img_files:
-    filename = img_file.split("/")[2].split("_")[0] + "_" + img_file.split("/")[-1]
+    filename = img_file.split("\\")[-2].split("_")[0] + "_" + img_file.split("\\")[-1]
     shutil.copy2(img_file, os.path.join(dest_dir, filename))
